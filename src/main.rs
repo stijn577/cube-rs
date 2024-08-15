a37e974e7743655d8d8c229ba02189e0a30ed397
stijn577 <stijnschotte03@gmail.com>
2024-08-15 21:51:32 +0200

    Add basics
    Provides the basics for creating the <libname>-sys crate where bindings
    will end up. The rest that is left will be making sure the headers are
    included correctly. This will be the most painful part of the project.

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,34 @@
-use anyhow::{Context, Result};
+use anyhow::Result;
 use clap::Parser;
-use std::{
-    fs::FileType,
-    path::{Path, PathBuf},
-};
+use functions::{create_build_rs, create_sys_crate, find_source_files};
+use std::path::PathBuf;
 
+#[macro_use]
+mod macros;
+mod functions;
+
+/// cube is a helper for generating Rust bindings for ST Cube HAL C code. Use cube --help to learn more.
 #[derive(Parser)]
 struct Args {
     /// name of -sys crate, you don't need to add -sys in the argument, simply the name of your crate. For example inputting cube, outputs a cube-sys crate.
     name: String,
     /// Path to the Makefile, this is used to read the C_SOURCES section of the Makefile, so we can select what source files must be used
-    makefile: PathBuf,
+    cube_project: String,
 }
 
 fn main() -> Result<()> {
     let args = Args::parse();
 
-    let contents = std::fs::read_to_string(args.makefile)?;
+    if std::fs::read_dir(PathBuf::from(&args.name)).is_err() {
+        create_sys_crate(&args.name)?;
+    } else {
+        println!("{}-sys crate exists, ignoring cargo new...", &args.name);
+    }
 
-    let sources = find_source_files(contents)?;
+    println!("Parsing C_SOURCES from Makefile...");
+    let sources = find_source_files(&args.name, &args.cube_project)?;
+
+    create_build_rs(&args.name, sources)?;
 
     Ok(())
-}
-
-fn find_source_files(file_content: String) -> Result<Vec<String>> {
-    let vec = file_content
-        .lines()
-        .map(|line| line.to_owned())
-        .enumerate()
-        .collect::<Vec<_>>();
-
-    let start;
-    let end;
-
-    let mut i = 0;
-    while !vec[i].1.starts_with("C_SOURCES") {
-        i += 1;
-    }
-    start = i;
-
-    while vec[i].1.ends_with('\\') {
-        i += 1
-    }
-    end = i;
-
-    Ok(vec
-        .into_iter()
-        .filter(|(i, _)| start < *i && *i < end + 1)
-        .map(|(_, line)| line.trim_matches('\\').trim().to_owned())
-        .collect())
 }
